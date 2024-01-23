36096e78ace8c87a7d4c0a3263870374fcdafa3c
Rob Runka <robrunka@sprintbased.com>
2024-01-22 22:19:14 -0700

    ttest

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {  
-  response.send(`TEST: ${process.env.TEST_ROUTE}`)
+  response.send(`TEST: ${process.env['super-secret']}`)
 })
 
 app.listen(app.get('port'), function() {

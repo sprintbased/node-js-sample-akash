f98e0c0abca8c990e8b953d1bc93a753662e0854
Rob Runka <robrunka@sprintbased.com>
2024-01-22 21:28:11 -0700

    test adding env var

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ var app = express()
 app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  console.debug("we hit this route!")
-  response.send(JSON.stringify(request.headers))
+app.get('/', function(request, response) {  
+  response.send(process.env.TEST_ROUTE)
 })
 
 app.listen(app.get('port'), function() {

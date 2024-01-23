49db0b8999e9189f6970a97cd8a6a99bc8d91b22
Rob Runka <robrunka@sprintbased.com>
2024-01-23 09:58:31 -0700

    test

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {  
-  response.send(`TEST: ${process.env.pass}`)
+  console.debug(process.env)
+  response.send(`TEST: ${process.env}`)
 })
 
 app.listen(app.get('port'), function() {

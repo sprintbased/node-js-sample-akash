c1f521278741c9749ece4d4687e0f55d673fa162
Rob Runka <robrunka@sprintbased.com>
2024-01-22 21:19:57 -0700

    fix typo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   console.debug("we hit this route!")
-  response.send(JSON.stringify(req.headers))
+  response.send(JSON.stringify(request.headers))
 })
 
 app.listen(app.get('port'), function() {

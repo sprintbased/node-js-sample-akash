27e0612fb916c2c11a216618a203305b0655767e
Rob Runka <robrunka@sprintbased.com>
2024-01-30 21:41:32 -0700

    test

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/healthz', function(request, response) {    
-  
-  response.send("success")
+  console.debug("Health check passed!")
+  response.send("Health check passed!")
 })
 
 app.get('/', function(request, response) {      
-  response.send(JSON.stringify(process.env.API_DOMAIN))
+  console.debug("Hello World!")
+  response.send("Hello World!")
 })
 
-app.listen(app.get('port'), function() {
-  console.log("API_DOMAIN:",process.env.API_DOMAIN)
+app.listen(app.get('port'), function() {  
   console.log("Node app is running at localhost:" + app.get('port'))
 })
